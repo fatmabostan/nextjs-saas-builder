a9f248a779d873e002abd712fd18c0cf9578e97b
fatmabostan <fatma_bostan_@outlook.com>
2025-02-18 23:09:32 +0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+
+
+const font = DM_Sans({  subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "FluXie",
+  description: "Effortlessly automate workflows, enhance flexibility, and boost efficiency with Fluxie.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${font.className}` }
+      > 
+      <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+      </body>
+    </html>
+  );
+}
